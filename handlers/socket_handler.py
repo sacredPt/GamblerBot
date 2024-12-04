7cd7e62c713067db7301458e4a02ff6ee1b5530c
sacredPt <qwertypavel656@gmail.com>
2024-12-04 10:42:36 +0300

    socket to async 2333

--- handlers/socket_handler.py
+++ handlers/socket_handler.py
@@ -1,57 +1,67 @@
+import asyncio
+from pprint import pprint
 import socketio
 import sys, os
 import time
+import logging
+import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import config
 import utils
 
 
-sio = socketio.Client()
+sio = socketio.AsyncClient()
 
 @sio.event
-def connect():
+async def connect():
     print("Connected successfully")
 
 @sio.event
-def connect_error(data):
+async def connect_error(data):
     print("Failed to connect:", data)
 
 @sio.event
-def disconnect():
+async def disconnect():
     print("Disconnected from server")
-    
 
 @sio.event
-def newDeposit(data):
+async def newDeposit(data):
     print("New deposit!")
-    utils.send_newDeposit(data)
-    sio.emit('DepositReceived', data)
+    await utils.send_newDeposit(data)
+    await sio.emit('DepositReceived', data)
 
-def main():
-    max_retries = 20000 # максимальное количество попыток
-    delay = 5        # задержка в секундах между попытками
+
+async def main():
+    max_retries = 20000
+    delay = 5
 
     for attempt in range(1, max_retries + 1):
         try:
             print(f"Attempt {attempt} to connect...")
-            sio.connect(
+            await sio.connect(
                 'https://gambler-panel.com',
                 headers={
                     'Authorization': f'Worker {config.API_TOKEN}',
-			        'x-connection-type': 'bot'
+                    'x-connection-type': 'bot'
                 },
                 socketio_path='/api/socket.io'
             )
-            break 
+            break
         except Exception as e:
             print(f"Connection attempt {attempt} failed: {e}")
-            time.sleep(delay)
+            if attempt < max_retries:
+                await asyncio.sleep(delay)
+            else:
+                print("Max retries reached. Could not connect.")
+                break
 
-def run():
-    main()
-    sio.wait()
+
+async def run():
+    await main()
+    await sio.wait()
+
 
 if __name__ == "__main__":
-    main()
-    sio.wait()
+    asyncio.run(run())
+
